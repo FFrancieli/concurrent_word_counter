952f9ad5cd23a379ff550b4f0bfe7ec08f6b0025
Francieli Viane <francielifrv@gmail.com>
2017-12-04 21:07:37 +0100

    remove unused import

--- src/main/java/fileReader/FileReaderService.java
+++ src/main/java/fileReader/FileReaderService.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class FileReaderService {

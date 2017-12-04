93e328084b91c242454211b71d7278370fa67972
Francieli Viane <francielifrv@gmail.com>
2017-12-04 19:31:13 +0100

    Remove logic from main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import wordCounter.FileReaderServiceService;
+import wordCounter.FileReaderService;
 
 import java.io.File;
 import java.io.IOException;
@@ -7,9 +7,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         File firstFile = new File("src/main/resources/first_file.txt");
         File seconFile = new File("src/main/resources/second_file.txt");
-
-        FileReaderServiceService service = new FileReaderServiceService(firstFile, seconFile);
-        service.countWordFrequencyFromFiles();
 
         System.out.println("done!");
     }

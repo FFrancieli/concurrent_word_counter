ed0f9079cda4bd0f08a899947b2956f4574ab557
Francieli Viane <francielifrv@gmail.com>
2017-12-05 00:54:02 +0100

    prints words frequencies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,16 @@
 import cache.Cache;
 import fileReader.FileReaderService;
+import word.Word;
+import wordCounter.WordCounterService;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+        public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
         File firstFile = new File("src/main/resources/first_file.txt");
         File seconFile = new File("src/main/resources/second_file.txt");
         List<File> files = Arrays.asList(firstFile, seconFile);
@@ -16,8 +19,12 @@ public class Main {
         FileReaderService fileReaderService = new FileReaderService(files, cache);
         fileReaderService.readFile();
 
-        System.out.println("done!");
+        Thread.sleep(100);
 
+        WordCounterService wordCounterService = new WordCounterService(cache);
+        List<Word> wordFrequency = wordCounterService.countWordsFrequency();
+
+        wordFrequency.forEach(System.out::println);
 
         System.out.println(cache.getCache().size());
     }

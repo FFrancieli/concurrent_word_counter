816ebfae4fa43d7500909081b209c8fcc1a03ed7
Francieli Viane <francielifrv@gmail.com>
2017-12-05 00:16:47 +0100

    counts words frequency

--- src/main/java/wordCounter/WordCountTask.java
+++ src/main/java/wordCounter/WordCountTask.java
@@ -6,20 +6,28 @@ import java.util.List;
 import java.util.concurrent.Callable;
 
 public class WordCountTask implements Callable {
+    private WordCounter wordCounter;
     private List<String> firstWordList;
     private List<String> secondWordList;
 
     public WordCountTask(List<String> firstWordList, List<String> secondWordList) {
         this.firstWordList = firstWordList;
         this.secondWordList = secondWordList;
+        this.wordCounter = new WordCounter(firstWordList, secondWordList);
     }
 
     public WordCountTask() {
     }
 
+    WordCountTask(List<String> firstWordList, List<String> secondWordList, WordCounter wordCounter) {
+        this.firstWordList = firstWordList;
+        this.secondWordList = secondWordList;
+        this.wordCounter = wordCounter;
+    }
+
     @Override
     public List<Word> call() throws Exception {
-        return null;
+        return wordCounter.countWords();
     }
 
     public boolean canBeExecuted() {
--- src/test/java/wordCounter/WordCountTaskTest.java
+++ src/test/java/wordCounter/WordCountTaskTest.java
@@ -1,16 +1,30 @@
 package wordCounter;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import word.Word;
 
 import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class WordCountTaskTest {
 
     public static final List<String> WORD_LIST = Collections.singletonList("word");
+
+    @Mock
+    WordCounter wordCounter;
+
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+    }
 
     @Test
     public void taskCannotBeExecutedWhenFirstWordListIsNull() throws Exception {
@@ -38,5 +52,26 @@ public class WordCountTaskTest {
         WordCountTask task = new WordCountTask(WORD_LIST, WORD_LIST);
 
         assertThat(task.canBeExecuted(), is(true));
+    }
+
+    @Test
+    public void countsWordFrequency() throws Exception {
+        WordCountTask wordCountTask = new WordCountTask(WORD_LIST, WORD_LIST, wordCounter);
+
+        wordCountTask.call();
+
+        verify(wordCounter).countWords();
+    }
+
+    @Test
+    public void returnsWordFrequency() throws Exception {
+        List<Word> words = Collections.singletonList(new Word("word", 1, 1));
+        when(wordCounter.countWords()).thenReturn(words);
+
+        WordCountTask wordCountTask = new WordCountTask(WORD_LIST, WORD_LIST, wordCounter);
+
+        List<Word> wordFrequency = wordCountTask.call();
+
+        assertThat(wordFrequency, is(words));
     }
 }
